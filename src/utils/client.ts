29ae2ecaa1792202c6d441adc87d0418704f4e6e
sayanta-nandi <sayantanandi123@gmail.com>
2025-03-25 20:08:25 +0530

    bug fix 10

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+declare global {
+  var prisma: ReturnType<typeof prismaClientSingleton>;
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = global.prisma || prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export { prisma };
